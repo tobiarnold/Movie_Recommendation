fbd02101bf2a4e45bbba30506e9a03f0a99e2d3b
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2023-08-04 15:27:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,30 +6,32 @@ def main():
         df = pd.read_csv(r"https://raw.githubusercontent.com/tobiarnold/Movie_Recommendation/main/movies.csv")
         st.set_page_config(page_title="Movie Recommendation", page_icon="🎬", layout="wide")
         streamlit_style = """
-            <style>
-            div.block-container{padding-top:0rem;}
-            [data-testid="stToolbar"]    {visibility: hidden;
-                                            height: 0%;
-                                            position: fixed;}
-            [data-testid="stHeader"] {background: rgba(0,0,0,0);}
-            .stApp {
-                background-image: url("https://raw.githubusercontent.com/tobiarnold/Movie_Recommendation/main/sl_121019_25870_69.jpg");
-                background-attachment: fixed;
-                background-size: cover;}
-             .title-wrapper {
-                text-align: center;}
-            .css-16idsys.e1nzilvr4 p {
-                color: blue; 
-                font-size: 22px;}
-            [data-testid="stTickBarMin"] {color: white;}
-            [data-testid="stTickBarMax"] {color: white;}
-             .css-7ym5gk.ef3psqc11 button {font-weight: bold;}
-             .css-1vbkxwb.e1nzilvr4 p {font-size: 22px;color: blue; }
-             .css-165ax5l.e1q9reml3 {color: white;}
-             .css-a51556.e1q9reml1 {color: white;}
-             .css-y4bq5x.ewgb6651 {color: white;}
-            </style>
-        """
+               <style>
+               div.block-container{padding-top:0rem;}
+               [data-testid="stToolbar"]    {visibility: hidden;
+                                               height: 0%;
+                                               position: fixed;}
+               [data-testid="stHeader"] {background: rgba(0,0,0,0);}
+               .stApp {
+                   background-image: url("https://raw.githubusercontent.com/tobiarnold/Movie_Recommendation/main/sl_121019_25870_69.jpg");
+                   background-attachment: fixed;
+                   background-size: cover;}
+                .title-wrapper {
+                   text-align: center;}
+               .css-16idsys.e1nzilvr4 p {
+                   color: blue; 
+                   font-size: 22px;}
+               [data-testid="stTickBarMin"] {color: white;}
+               [data-testid="stTickBarMax"] {color: white;}
+               [data-testid="stText"] {font-size: 18px}
+                .css-7ym5gk.ef3psqc11 button {font-weight: bold;}
+                .css-1vbkxwb.e1nzilvr4 p {font-size: 22px;color: blue; }
+                .css-165ax5l.e1q9reml3 {color: white;}
+                .css-a51556.e1q9reml1 {color: white;}
+                .css-y4bq5x.ewgb6651 {color: white;}
+                .css-5rimss.e1nzilvr4 {color: white;}
+               </style>
+           """
         st.markdown(streamlit_style, unsafe_allow_html=True)
         # st.title("🎬 Random Movie Recommendation",container_style={'text-align': 'center'})
         st.markdown("<h1 style='text-align: center;color: white;'>🎬 Random Movie Recommendation</h1>",
@@ -52,8 +54,10 @@ def main():
         try:
             with col2:
                 text_title=df_title["title"]
-                st.text(df_title["title"].iloc[0])
-            df_table=df_title.drop("title", axis=1)
+                st.text(text_title.iloc[0])
+                text_overview=df_title["overview"]
+            st.write(text_overview.iloc[0])
+            df_table = df_title.drop(["title","overview"], axis=1)
             st.dataframe(df_table)
         except:
             st.text("press button to generate random movie recommendations")
