553759763d6fbe1e8f847e778cc44f9a69c0e474
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2023-08-04 12:34:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def main():
                 df = df[(df["release_date"] >= year[0]) & (df["release_date"] <= year[1])]
         try:
             st.text("on mobile devices, please double-click the table column to view the full text")
-            st.write(df.sample(n=1))
+            st.write(df["title"].sample(n=1))
         except:
             st.text("Please choose other parameters!")
     except:
